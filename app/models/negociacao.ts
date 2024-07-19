168f8192e1df6639cb8e240693da15d0e3610673
Rômulo Deyvid <romulo.deyvid@hotmail.com>
2024-07-19 11:54:56 -0300

    Simplificando o código

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,23 +1,15 @@
 export class Negociacao{
-    private _data: Date
-    private _quantidade: number
-    private _valor: number
-
-    constructor(data: Date, quantidade: number, valor: number){
-        this._data = data
-        this._quantidade = quantidade
-        this._valor = valor
-    }
-    get data(): Date{
-        return this._data
-    }
-    get quantidade(): Number{
-        return this._quantidade
-    }
-    get valor(): Number{
-        return this._valor
+    
+    constructor(
+       private _data: Date, 
+       public readonly quantidade: number, 
+       public readonly valor: number
+    ){}
+    get data(): Date {
+        const data = new Date(this._data.getTime())
+        return data
     }
     get volume(): Number{
-        return this._quantidade * this._valor
+        return this.quantidade * this.valor
     }
 }
